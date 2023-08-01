9561eb6858358d0e4a0fa747e9540538b32f5a96
Alberto Villanueva <albertt.villanueva@gmail.com>
2023-07-31 20:13:24 -0700

    produccion

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -7,23 +7,23 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class LoginService {
-
+   url:string ="https://689f-2806-101e-7-2565-4807-3846-fcef-dcf1.ngrok-free.app";
   constructor(private http:HttpClient) { }
 
   public login(email:string, password:string):Observable<any>{
-    return this.http.post(environment.url + '/login', {email,password});
+    return this.http.post(this.url + '/login', {email,password});
   }
 
   public user():Observable<any>{
-    return this.http.post(environment.url + '/user',{});
+    return this.http.post(this.url + '/user',{});
   }
 
   public sendEmail(email:string):Observable<any>{
-    return this.http.post(environment.url + '/forgot', {email});
+    return this.http.post(this.url + '/forgot', {email});
   }
 
   public reset(token:string, password:string, password_confirm:string):Observable<any>{
-    return this.http.post(environment.url + '/reset', {token,password,password_confirm});
+    return this.http.post(this.url + '/reset', {token,password,password_confirm});
   }
 
 }
